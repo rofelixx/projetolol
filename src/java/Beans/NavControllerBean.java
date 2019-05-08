86a669c20e438cfc3c115249db9a5cb9d2ab10c8
ALM4CT <ALM4CT@CTPC3569.br.bosch.com>
2019-05-08 16:53:41 -0300

    Commit change menu, adding bootsfaces to project, recreate login form

---
+++ src/java/Beans/NavControllerBean.java
@@ -0,0 +1,58 @@
+package Beans;
+
+import Classe.Cliente;
+import Classe.Endereco;
+import DAO.ClienteDao;
+import DAO.Criptografia;
+import java.math.BigInteger;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import javax.faces.application.FacesMessage;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+import javax.faces.context.FacesContext;
+import javax.servlet.http.HttpSession;
+
+@ManagedBean(name = "NavMB")
+@SessionScoped
+
+public class NavControllerBean {
+
+    public boolean isLogged = false;
+
+    public String home() {
+        return "home.xhtml";
+    }
+
+    public String index() {
+        return "index.xhtml";
+    }
+
+    public String signUp() {
+        return "cadastro.xhtml";
+    }
+
+    public String novoPedido() {
+        return "novoPedido.xhtml";
+    }
+
+    public String pedidos() {
+        return "pedidos.xhtml";
+    }
+
+    public String gerenciarPedidos() {
+        return "gerenciarPedidos.xhtml";
+    }
+
+    public String gerenciarClientes() {
+        return "gerenciarClientes.xhtml";
+    }
+
+    public String gerenciarRoupas() {
+        return "gerenciarRoupas.xhtml";
+    }
+
+    public boolean isLogged() {
+        return isLogged;
+    }
+}
