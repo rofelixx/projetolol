062706d4cef493f13a94808925e9e420a9ea8a13
ALM4CT <ALM4CT@CTPC3569.br.bosch.com>
2019-05-16 11:47:35 -0300

    Commit Add ClienteMB and new screen, remove folders useless

--- src/java/Beans/ClienteManagedBean.java
+++ src/java/Beans/ClienteManagedBean.java
@@ -1,32 +1,21 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package Beans;
 
 import Classe.Cliente;
 import DAO.ClienteDao;
-import java.util.ArrayList;
 import java.util.List;
+import javax.faces.bean.ManagedBean;
 import javax.faces.bean.SessionScoped;
-import javax.inject.Named;
 
-/**
- *
- * @author Guigo
- */
-@Named(value = "ClienteMB")
+@ManagedBean(name = "ClienteMB")
 @SessionScoped
+
 public class ClienteManagedBean {
 
+    public Cliente cliente = new Cliente();
     public ClienteDao dao = new ClienteDao();
-    public List<Cliente> lista = new ArrayList<>();
 
     public List<Cliente> getAllClientes() throws Exception {
-        for (Cliente clienteEntity : dao.getAll()) {
-            lista.add(new Cliente(clienteEntity.getNome(), clienteEntity.getEmail(), clienteEntity.getCpf(), clienteEntity.getSexo(), clienteEntity.getSenha()));
-        }
-        return lista;
+        List<Cliente> retorno = dao.getAll();
+        return retorno;
     }
 }
