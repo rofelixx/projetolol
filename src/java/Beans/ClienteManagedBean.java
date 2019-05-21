33c503c90bb9a6793b87ab8908b77a2365423ec7
ALM4CT <ALM4CT@CTPC3569.br.bosch.com>
2019-05-21 14:51:12 -0300

    Commit format gerenciar clientes, add sweetalert to question

--- src/java/Beans/ClienteManagedBean.java
+++ src/java/Beans/ClienteManagedBean.java
@@ -4,9 +4,11 @@ import Classe.Cliente;
 import DAO.ClienteDao;
 import java.io.Serializable;
 import java.util.List;
+import javax.faces.context.FacesContext;
 import javax.faces.view.ViewScoped;
 import javax.inject.Named;
 import net.bootsfaces.utils.FacesMessages;
+import org.primefaces.PrimeFaces;
 
 @ViewScoped
 @Named("ClienteMB")
@@ -16,13 +18,13 @@ public class ClienteManagedBean implements Serializable {
     private ClienteDao dao = new ClienteDao();
     private String url = "";
     private NavControllerBean nav = new NavControllerBean();
-    
+
     public List<Cliente> getAllClientes() throws Exception {
         List<Cliente> retorno = dao.getAll();
         return retorno;
     }
 
-    public String salvarEdicao() {        
+    public String salvarEdicao() {
         boolean saved = dao.editCliente(cliente);
         if (saved) {
             url = nav.gerenciarClientes();
@@ -39,5 +41,26 @@ public class ClienteManagedBean implements Serializable {
 
     public void setCliente(Cliente cliente) {
         this.cliente = cliente;
+    }
+
+    public void confirmDelete() {
+        PrimeFaces.current().executeScript("Swal.fire({  "
+                + "title: \"Voce tem certeza?\",\n"
+                + "  text: \"Voce irá exluir um registro.\",\n"
+                + "  type: \"warning\",\n"
+                + "  showCancelButton: true,\n"
+                + "  confirmButtonColor: \"#DD6B55\",\n"
+                + "  confirmButtonText: \"Deletar\",\n"
+                + "  cancelButtonText: \"Cancelar\",\n"
+                + "  closeOnConfirm: false,\n"
+                + "  closeOnCancel: false\n"
+                + "},\n"
+                + "function(isConfirm){\n"
+                + "  if (isConfirm) {\n"
+                + "    form.submit();          // submitting the form when user press yes\n"
+                + "  } else {\n"
+                + "    swal(\"Cancelled\", \"Your imaginary file is safe :)\", \"error\");\n"
+                + "  }\n"
+                + "})");
     }
 }
