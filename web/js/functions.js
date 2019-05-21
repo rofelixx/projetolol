70c3e18c7886f9beee5be37569acca4b6f064fb7
Guigo <Guigo@DESKTOP-ONTE0N8>
2019-05-20 23:43:19 -0300

    Commit gerenciarClientes change table for bootsfaces table

--- web/js/functions.js
+++ web/js/functions.js
@@ -77,14 +77,5 @@ $(document).ready(function () {
 
     });
 
-    $("#myInput").on("keyup", function () {
-        var value = $(this).val().toLowerCase();
-        $("#myTable tr").filter(function () {
-            $(this).toggle($(this).text().toLowerCase().indexOf(value) > -1);
-        });
-    });
-
     $("#dtNascDatepicker").datepicker();
-    $("#startdate_datepicker").datepicker();
-
 });
